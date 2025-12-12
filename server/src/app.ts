f0b7509bb003591fb747d58cc2f7392c80e78126
Srinanth <srinanthmv845@gmail.com>
2025-12-13 01:35:24 +0530

    cors updated,limiters and error checking added

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,11 +2,21 @@ import express from 'express';
 import cors from 'cors';
 import spaceRoutes from './routes/space.routes.js';
 import fileRoutes from './routes/file.routes.js';
+import { errorHandler } from './middleware/error.js';
+import { globalLimiter } from './config/ratelimiter.js';
 
 const app = express();
 
-app.use(cors());
+app.set('trust proxy', 1);
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-public-id'],
+  exposedHeaders: ['x-public-id']
+}));
 app.use(express.json());
+app.use(globalLimiter);
 
 // Routes
 app.use('/api/spaces', spaceRoutes);
@@ -16,4 +26,5 @@ app.get('/', (_req, res) => {
   res.send('TempSpace API is running');
 });
 
+app.use(errorHandler);
 export default app;
