e4f74f04b3080335859a415a0b89cc2bb5dd75b7
Srinanth <srinanthmv845@gmail.com>
2025-12-13 01:37:15 +0530

    abstracted functions

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,11 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import { bootstrap } from './bootstrap.js';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+bootstrap();
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
