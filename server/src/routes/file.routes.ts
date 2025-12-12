8ab0c2582c4adc990a96729ec2c799fb837e9536
Srinanth <srinanthmv845@gmail.com>
2025-12-13 01:30:54 +0530

    all routes added.

--- server/src/routes/file.routes.ts
+++ server/src/routes/file.routes.ts
@@ -2,15 +2,14 @@ import { Router } from 'express';
 import { getUploadUrl, confirmUpload, listFiles,downloadFile } from '../controller/file.controller.js';
 import { requirePerms } from '../middleware/perms.js';
 import { Perms } from '../types/perms.js';
+import { validate } from '../middleware/validate.js';
+import { uploadUrlSchema, confirmUploadSchema, downloadSchema } from '../schema/file.schema.js';
 
 const router = Router();
 
-router.post('/upload-url', requirePerms(Perms.UPLOAD), getUploadUrl);
-
-router.post('/confirm', requirePerms(Perms.UPLOAD), confirmUpload);
-
+router.post('/upload-url',requirePerms(Perms.UPLOAD),validate(uploadUrlSchema),getUploadUrl);
+router.post('/confirm',requirePerms(Perms.UPLOAD),validate(confirmUploadSchema),confirmUpload);
 router.get('/', requirePerms(Perms.READ), listFiles);
-
-router.get('/:fileId/download', requirePerms(Perms.READ), downloadFile);
+router.get('/:fileId/download',requirePerms(Perms.READ),validate(downloadSchema),downloadFile);
 
 export default router;
--- server/src/routes/space.routes.ts
+++ server/src/routes/space.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from 'express';
-import { createSpace, getSpace, deleteSpace } from '../controller/space.controller.js';
+import { createSpace, getSpace, deleteSpace, updateSettings,createPublicLink } from '../controller/space.controller.js';
 import { requirePerms } from '../middleware/perms.js';
 import { Perms } from '../types/perms.js';
+import { createSpaceLimiter } from '../config/ratelimiter.js';
+import { validate } from '../middleware/validate.js';
+import { updateSettingsSchema } from '../schema/space.schema.js';
 
 const router = Router();
 
-router.post('/', createSpace);
+router.post('/', createSpaceLimiter, createSpace);
 
 router.get('/', requirePerms(Perms.READ), getSpace);
 router.delete('/', requirePerms(Perms.CONFIG), deleteSpace);
+router.patch('/',requirePerms(Perms.CONFIG),validate(updateSettingsSchema),updateSettings);
+router.post('/share', requirePerms(Perms.CONFIG), createPublicLink);
 
 export default router;
