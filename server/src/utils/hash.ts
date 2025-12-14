96da46a48c401a960f04d839c8a78daa2b278e8d
Srinanth <srinanthmv845@gmail.com>
2025-12-14 21:47:48 +0530

    hash for password protected space added.

--- server/src/utils/hash.ts
+++ server/src/utils/hash.ts
@@ -1,5 +1,26 @@
-import crypto from 'crypto';
+import crypto,{ scrypt, randomBytes, timingSafeEqual } from 'crypto';
+import { promisify } from 'util';
+
+const scryptAsync = promisify(scrypt);
 
 export const hashToken = (token: string): string => {
   return crypto.createHash('sha256').update(token).digest('hex');
+};
+
+export const hashPassword = async (password: string): Promise<string> => {
+    const salt = randomBytes(16).toString('hex');
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+    
+    return `${salt}:${buf.toString('hex')}`;
+};
+
+export const verifyPassword = async (password: string, storedHash: string): Promise<boolean> => {
+    const [salt, key] = storedHash.split(':');
+    
+    if (!salt || !key) return false;
+
+    const keyBuffer = Buffer.from(key, 'hex');
+    const derivedKey = (await scryptAsync(password, salt, 64)) as Buffer;
+
+    return timingSafeEqual(keyBuffer, derivedKey);
 };
