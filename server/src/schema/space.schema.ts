d732065c3ba9b3211d0e9e029d41af1d8d002fda
Srinanth <srinanthmv845@gmail.com>
2025-12-14 21:46:33 +0530

    schema updated

--- server/src/schema/space.schema.ts
+++ server/src/schema/space.schema.ts
@@ -5,11 +5,19 @@ export const updateSettingsSchema = z.object({
     public_upload: z.boolean().optional(),
     download_allowed: z.boolean().optional(),
     password_protected: z.boolean().optional(),
+    password: z.string().min(1, "Password cannot be empty").optional()
   }).strict(),
 });
 
 export const joinSpaceSchema = z.object({
   body: z.object({
     code: z.string().min(1).max(25),
+  }),
+});
+
+export const unlockSpaceSchema = z.object({
+  body: z.object({
+    spaceId: z.string().uuid(),
+    password: z.string().min(1),
   }),
 });
