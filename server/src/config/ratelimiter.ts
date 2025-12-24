002f9dddb379625d8dfea234f1ec9d94b86bdef0
Srinanth <srinanthmv845@gmail.com>
2025-12-24 23:42:22 +0530

    fix: update CORS origin for security and enhance rate limiter settings for better request handling

--- server/src/config/ratelimiter.ts
+++ server/src/config/ratelimiter.ts
@@ -1,15 +1,15 @@
 import rateLimit from 'express-rate-limit';
 
 export const globalLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 100,
+  windowMs: 2 * 60 * 1000, 
+  max: 500,
   message: 'Too many requests from this IP, please try again later.',
   standardHeaders: true, 
   legacyHeaders: false, 
 });
 
 export const createSpaceLimiter = rateLimit({
-  windowMs: 60 * 60 * 1000, 
+  windowMs: 5 * 60 * 1000, 
   max: 5,
-  message: 'You have created too many spaces recently.',
+  message: 'You have created too many spaces recently, try again after 5 minutes!',
 });
